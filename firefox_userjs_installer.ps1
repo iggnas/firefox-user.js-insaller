9393b9415a248e165204a4064d7aab696326c6f1
iggnas <122698562+iggnas@users.noreply.github.com>
2024-04-07 08:38:24 +0000

    Update firefox_userjs_installer.ps1
    rewrite code and add more user.js options

--- firefox_userjs_installer.ps1
+++ firefox_userjs_installer.ps1
@@ -1,3 +1,5 @@
+function Write-HostCenter { param($Message) Write-Host ("{0}{1}" -f (' ' * (([Math]::Max(0, $Host.UI.RawUI.BufferSize.Width / 2) - [Math]::Floor($Message.Length / 2)))), $Message) }
+
 $ProfilePath = (Get-ChildItem "$env:APPDATA\Mozilla\Firefox\Profiles" | Where-Object { $_.Name -like "*default-release*" }).FullName
 
 if ($ProfilePath) {
@@ -6,40 +8,47 @@ if ($ProfilePath) {
     Write-Host "Firefox profile directory was not found."
 }
 
+$UserJsFiles = @{
+    '1' = @{ 'url' = 'https://raw.githubusercontent.com/iggnas/firefox-user.js-insaller/main/user.js'; 'outputFile' = "$ProfilePath\user2.js" }
+    '2' = @{ 'url' = 'https://raw.githubusercontent.com/arkenfox/user.js/master/user.js'; 'outputFile' = "$ProfilePath\user2.js" }
+    '3' = @{ 'url' = 'https://raw.githubusercontent.com/yokoffing/Betterfox/main/user.js'; 'outputFile' = "$ProfilePath\user2.js" }
+    '4' = @{ 'url' = 'https://raw.githubusercontent.com/pyllyukko/user.js/master/user.js'; 'outputFile' = "$ProfilePath\user2.js" }
+    '5' = @{ 'url' = 'https://raw.githubusercontent.com/iggnas/firefox-user.js-insaller/main/NarsilFork.js'; 'outputFile' = "$ProfilePath\user2.js" }
+}
+
+function DownloadUserJs($url, $outputFile) {
+    try {
+        Invoke-WebRequest $url -OutFile $outputFile
+        Write-Host "Installation completed successfully"
+    } catch {
+        Write-Host "An error occurred during installation: $_.Exception.Message"
+    }
+}
+
 $MainMenu = {
     Clear-Host
-    Write-Host "_______________ Firefox user.js Installer _______________"
-    Write-Host "_______________________ by iggnas _______________________"
-    Write-Host "`n"
-    Write-Host "`n"
-    Write-Host "1. Install Narsil user.js"
-    Write-Host "2. Install Arkenfox user.js"
-    Write-Host "3. Install Betterfox user.js"
-    Write-Host "4. Quit"
+    Write-HostCenter '______________ Firefox user.js Installer ______________'
+    Write-HostCenter '______________________ by iggnas ______________________'
+    Write-Host "`n`n"
+    Write-HostCenter "[1] Install Narsil user.js        [2] Install Arkenfox user.js"
+	Write-Host "`n"
+    Write-HostCenter "[3] Install Betterfox user.js     [4] Install pyllyukko user.js"
+	Write-Host "`n"
+    Write-HostCenter "[5] Install my fork of Narsil user.js"
 }
 
 while ($true) {
     &$MainMenu
-    $Selection = Read-Host "Make a selection: "
+    Write-Host "`n"
+    $Selection = Read-Host "Make a selection"
     
-    switch ($Selection) {
-        '1' {
-            Write-Host "Installing Narsil user.js"
-            Start-Sleep -Seconds 1
-            Invoke-WebRequest "https://codeberg.org/Narsil/user.js/raw/branch/main/desktop/user.js" -OutFile "$ProfilePath\user.js"
-        }
-        '2' {
-            Write-Host "Installing Arkenfox user.js"
-            Start-Sleep -Seconds 1
-            Invoke-WebRequest "https://raw.githubusercontent.com/arkenfox/user.js/master/user.js" -OutFile "$ProfilePath\user.js"
-        }
-        '3' {
-            Write-Host "Installing Betterfox user.js"
-            Start-Sleep -Seconds 1
-            Invoke-WebRequest "https://raw.githubusercontent.com/yokoffing/Betterfox/main/user.js" -OutFile "$ProfilePath\user.js"
-        }
-        '4' {
-            break
-        }
+    if ($UserJsFiles.ContainsKey($Selection)) {
+        $userJs = $UserJsFiles[$Selection]
+		Write-Host "`n"
+        Write-Host "Installing $($userJs.url)"
+        Start-Sleep -Seconds 1
+        DownloadUserJs $userJs.url $userJs.outputFile
+    } else {
+        Write-Host "Invalid selection. Please choose a valid option."
     }
 }
